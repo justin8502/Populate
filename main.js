f7bbd10400795e8155cee63ae3c59fa268b0e4b9
justinyijinang <jya005@ucsd.edu>
2017-03-08 20:24:17 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
+var animateCir = function() {
+    $("#populationCirc").animate({
+        width: 120,
+        height: 120,
+        top: '+= 100',
+        left: '44%',
+	}); 
+    $("#populationCirc").animate({
+        width: 100,
+        height: 100,
+        top: '-= 100',
+        left: '45%'
+	});
+	//$("populationText").fadeOut(1000);
+	setTimeout(function() {animateCir() }, 5000);
+}
+
 $(document).ready(function() {
       $("button").click(function(){
-        $("#populationCirc").animate({
-		height: 160,
-		width: 160
-	   });
-        $("#populationCirc").animate({
-		height: 100,
-		width: 100
-	   });
-
+        animateCir();
     });
 });
