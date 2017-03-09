fa685589551aa514cbddeac23434b7bd50eff289
justinyijinang <jya005@ucsd.edu>
2017-03-08 21:26:03 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,37 @@
+var incycle = true;
+
+var outerbound = 120;
+
 var animateCir = function() {
+	if(incycle) {
     $("#populationCirc").animate({
-        width: 120,
-        height: 120,
+        width: outerbound,
+        height: outerbound,
         top: '+= 100',
         left: '44%',
-	}); 
+	}, 500); 
     $("#populationCirc").animate({
         width: 100,
         height: 100,
         top: '-= 100',
         left: '45%'
-	});
-	//$("populationText").fadeOut(1000);
+	}, 500);
+	$("#populationText").fadeOut(500);
+	$("#populationText").fadeIn(500);
 	setTimeout(function() {animateCir() }, 5000);
+	}
+}
+
+function outputUpdate(num) {
+    document.querySelector('#output').value = num;
+    document.getElementById('populationText').innerHTML = 'n=' + num;
+    //outerbound = num + 10;
 }
 
 $(document).ready(function() {
-      $("button").click(function(){
-        animateCir();
+	setTimeout(function() {animateCir() }, 5000);
+    $("button").click(function(){
+        incycle = !incycle;
     });
+
 });
