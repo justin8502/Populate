83a371aed177b56a08bc7b511a146fbc6bce1a70
justinyijinang <jya005@ucsd.edu>
2017-03-08 16:10:19 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+      $("button").click(function(){
+        $("#populationCirc").animate({
+		height: 160,
+		width: 160
+	   });
+        $("#populationCirc").animate({
+		height: 100,
+		width: 100
+	   });
+
+    });
+});
