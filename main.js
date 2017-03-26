3a468fffc3a5beda3d2e6a9835d97811e53199a8
Justin Ang <jya005@ucsd.edu>
2017-03-26 16:29:00 -0700

    bugfixes?

--- main.js
+++ main.js
@@ -2,20 +2,41 @@ var incycle = true;
 
 var outerbound = 120;
 
+var prevbound = outerbound;
+
+var tochange = 0;
+
 var animateCir = function() {
+	var offset = 10;
 	if(incycle) {
-    $("#populationCirc").animate({
-        width: outerbound,
-        height: outerbound,
-        top: '+= 100',
-        left: '44%',
-	}, 500); 
-    $("#populationCirc").animate({
-        width: 100,
-        height: 100,
-        top: '-= 100',
-        left: '45%'
-	}, 500);
+		if(prevbound != outerbound){
+   	 	$("#populationCirc").animate({
+        	width: prevbound,
+        	height: prevbound,
+        	left: '-=' + parseInt(offset),
+			}, 500); 
+   	 	prevbound = outerbound;
+		} else {
+   	 	$("#populationCirc").animate({
+        	width: outerbound,
+        	height: outerbound,
+        	left: '-=' + parseInt(offset),
+			}, 500); 
+   	 	}
+        if(parseInt(tochange) != 0) {
+    		$("#populationCirc").animate({
+        		width: outerbound - 20,
+        		height: outerbound - 20,
+        		left: '+=' + (parseInt(offset) + parseInt(tochange)),
+				}, 500);
+        } else {
+    		$("#populationCirc").animate({
+        		width: outerbound - 20,
+        		height: outerbound - 20,
+        		left: '+=' + parseInt(offset),
+				}, 500);
+        }
+	tochange = 0;
 	$("#populationText").fadeOut(500);
 	$("#populationText").fadeIn(500);
 	setTimeout(function() {animateCir() }, 5000);
@@ -23,9 +44,11 @@ var animateCir = function() {
 }
 
 function outputUpdate(num) {
+    tochange = ((outerbound-20)-parseInt(num))/8;
     document.querySelector('#output').value = num;
     document.getElementById('populationText').innerHTML = 'n=' + num;
-    //outerbound = num + 10;
+    outerbound = parseInt(num) + 20;
+    document.getElementById('populationText').innerHTML = tochange;
 }
 
 $(document).ready(function() {
